8d89360606969abc5a184a1ca8b4986692ff2baf
Richard Hanrahan <fahitch@gmail.com>
2023-04-02 22:51:07 +0100

    Adds success state to subscription component

--- src/components/EmailListForm.js
+++ src/components/EmailListForm.js
@@ -3,17 +3,22 @@ import addToMailchimp from 'gatsby-plugin-mailchimp'
 
 const EmailListForm = () => {
   const [email, setEmail] = useState('');
+  const [emailStatus, setEmailStatus] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     addToMailchimp(email) 
     .then(data => {
+        if (data.result === "success") {
+            setEmailStatus("success")
+        }
       // I recommend setting data to React state
       // but you can do whatever you want (including ignoring this `then()` altogether)
-      console.log(data)
+      // console.log(data)
     })
     .catch(() => {
+        setEmailStatus("failure")
       // unnecessary because Mailchimp only ever
       // returns a 200 status code
       // see below for how to handle errors
@@ -27,7 +32,7 @@ const EmailListForm = () => {
 
   return (
     <form onSubmit={handleSubmit} >
-      <h2>Subscribe to my email list!</h2>
+      <h2>Subscribe</h2>
       <div >
         <input
           placeholder="Email address"
@@ -37,6 +42,12 @@ const EmailListForm = () => {
         />
         <button type="submit">Subscribe</button>
       </div>
+      {emailStatus === 'success' && (
+        <div id="subscribe-success">Email successfully added to our mailing list!</div>
+        )}
+        {emailStatus === 'failure' && (
+            <div id="">Issue encountered, can you check your email is valid and try again</div>
+            )}
     </form>
   );
 };
